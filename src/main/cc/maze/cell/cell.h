a3ecdafe3455bfb29d657320ad88e9bfb5cbd4f0
Sourabh <marathe.so@husky.neu.edu>
2016-10-29 10:00:54 -0400

    Add basic class methods for Cell

--- src/main/cc/maze/cell/cell.h
+++ src/main/cc/maze/cell/cell.h
@@ -3,6 +3,28 @@
 
 // Abstract class to represent a cell.
 public class Cell {
+public:
+	// Creates a Cell object at (x,y)
+	Cell(int x, int y);
+
+	// Destructor 
+	~Cell(int x, int y);
+
+	// Gets the x offsest of a Cell
+	int getLocationX();
+
+	// Gets the y offset of a Cell
+	int getLocationY();
+
+	// Gets the status of whether a Cell has been visited
+	bool isVisited();
+
+
+private:
+	int x;
+	int y; 
+	bool visited; 
+
 	 	
 };
 
