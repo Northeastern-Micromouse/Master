85a44d1f4d59b36c28041e770a908af0c64f6e5c
Matthew <murphy1022@gmail.com>
2017-01-18 22:30:09 -0500

    More work on explore. Rename moves to direction

--- src/main/c/com/micromouse/robot/robot.h
+++ src/main/c/com/micromouse/robot/robot.h
@@ -3,7 +3,7 @@
 
 #include "src/main/c/com/micromouse/hardware/hardware.h"
 #include "src/main/c/com/micromouse/location/location.h"
-#include "src/main/c/com/micromouse/utility/moves/move.h"
+#include "src/main/c/com/micromouse/utility/direction/direction.h"
 #include "src/main/c/com/micromouse/maze/cell.h"
 
 typedef struct Robot {
@@ -14,6 +14,8 @@ typedef struct Robot {
 // Returns a robot location at the given location.
 Robot* InitializeRobot(Location* location);
 
+void CanMove(Robot *winslow, Move move);
+
 // Frees all of the memory associated with the given robot.
 void RobotDestructor(Robot* winslow);
 
@@ -21,6 +23,6 @@ void RobotDestructor(Robot* winslow);
 void ExploreMaze(Robot* winslow);
 
 // Updates the maze in the robot with the sensor information/
-void UpdateMaze(Robot* winslow, SR* values);
+void UpdateMaze(Robot* winslow, Move* values);
 
 #endif  // MICROMOUSE_ROBOT_ROBOT_H_
