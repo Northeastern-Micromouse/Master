ed9808ade01310b5877afaef2d5e8f58e81fa9b6
Matthew <murphy1022@gmail.com>
2017-02-01 19:26:11 -0500

    Add more function declarations

--- src/main/c/com/micromouse/utility/vector/cell_vector.h
+++ src/main/c/com/micromouse/utility/vector/cell_vector.h
@@ -7,12 +7,22 @@ typedef struct Vector_c {
   int size;
   int capacity;
   Cell** data;
+
+  bool tight;
 } Vector_c;
 
 Vector_c* make_vector_c();
 Vector_c* make_vector_c_with_size(int n);
 
 void print_vector_c(Vector_c*);
+
+int vector_c_length(Vector_c*);
+
+Cell* vector_c_front(Vector_c*);
+
+Cell* vector_c_back(Vector_c*);
+
+bool vector_c_empty(Vector_c*);
 
 void append_vector_c(Vector_c*, Cell* data);
 
