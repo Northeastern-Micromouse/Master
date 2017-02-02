10478d93b0084c669bc7762c25d4338213f09981
Matthew <murphy1022@gmail.com>
2017-02-02 16:19:00 -0500

    Add a window class and exit button

--- src/main/qt/default.pro
+++ src/main/qt/default.pro
@@ -3,6 +3,10 @@ TARGET = micromouse
 
 QT = core gui
 
-SOURCES +=  main.cpp
+SOURCES +=  main.cpp \
+    window.cpp
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+HEADERS += \
+    window.h
--- src/main/qt/main.cpp
+++ src/main/qt/main.cpp
@@ -1,7 +1,11 @@
 #include <QApplication>
+#include "window.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   QApplication app (argc, argv);
+
+  micromouse::graphics::Window w;
+  w.show();
+
   return app.exec();
 }
